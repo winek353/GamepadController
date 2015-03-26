8f8db6bc801c29640e55c378ab35645b30d21ddc
Dawid Winiarski <winiu31@gmail.com>
2015-03-26 16:37:42 +0100

    I did my best. Whats done is done. Trust me your project is fine :)

--- Source/DolphinShortcuts.cpp
+++ Source/DolphinShortcuts.cpp
@@ -16,42 +16,42 @@ void DolphinShortcuts::handleButton(JoypadButton button, PressedOrReleased press
 	  if(pressedOrReleased == PRESSED)
 		  systemController->pressKey(28);
 	  else
-		  systemController->pressKey(28);
+		  systemController->releaseKey(28);
 	  }
 	  if(button == BUTTON_B)
 	  {
 	  if(pressedOrReleased == PRESSED)
 		  systemController->pressKey(14);
 	  else
-		  systemController->pressKey(14);
+		  systemController->releaseKey(14);
 	  }
 	  if(button ==  BUTTON_LEFT)
 	  {
 	  if(pressedOrReleased == PRESSED)
 		  systemController->pressKey(105);
 	  else
-		  systemController->pressKey(105);
+		  systemController->releaseKey(105);
 	  }
 	  if(button ==  BUTTON_RIGHT)
 	  {
 	  if(pressedOrReleased == PRESSED)
 		  systemController->pressKey(106);
 	  else
-		  systemController->pressKey(106);
+		  systemController->releaseKey(106);
 	  }
 	  if(button ==  BUTTON_UP)
 	  {
 	  if(pressedOrReleased == PRESSED)
 		  systemController->pressKey(103);
 	  else
-		  systemController->pressKey(103);
+		  systemController->releaseKey(103);
 	  }
 	  if(button ==  BUTTON_DOWN)
 	  {
 	  if(pressedOrReleased == PRESSED)
 		  systemController->pressKey(108);
 	  else
-		  systemController->pressKey(108);
+		  systemController->releaseKey(108);
 	  }
 }
 
